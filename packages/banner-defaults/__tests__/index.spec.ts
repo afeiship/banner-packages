ad38e9b71f85c161bec4f47c36033be046e0e711
afeiship <1290657123@qq.com>
2022-08-15 11:25:10 +0800

    fix: rm undefined value

--- packages/banner-defaults/__tests__/index.spec.ts
+++ packages/banner-defaults/__tests__/index.spec.ts
@@ -8,14 +8,15 @@ describe('api.basic', () => {
     expect(res[3].includes('version: ')).toBeTruthy();
   });
 
-  test('provide pkg should get info from provider',()=>{
+  test('provide pkg should get info/missing will get undefined from provider', () => {
     const pkg = {
       name: '@jswork/banner-defaults-by-mock',
-      version: 'mock version'
+      version: 'mock version',
     };
 
     const res = fn(pkg);
     expect(res[0]).toBe('name: @jswork/banner-defaults-by-mock');
-    expect(res[3]).toBe('version: mock version');
-  })
+    expect(res.find((item) => item.includes('version:'))).toBe('version: mock version');
+    expect(res.find((item) => item.includes('description:'))).toBe(undefined);
+  });
 });
--- packages/banner-defaults/src/index.ts
+++ packages/banner-defaults/src/index.ts
@@ -1,15 +1,23 @@
 import path from 'path';
 import dateformat from 'dateformat';
+
 const DEFAULT_FORMAT = 'yyyy-mm-dd HH:MM:ss';
 
 export default (inPkg?): string[] => {
   const pkg = inPkg || require(path.join(process.cwd(), './package.json'));
-  return [
-    `name: ${pkg.name}`,
-    `description: ${pkg.description}`,
-    `homepage: ${pkg.homepage}`,
-    `version: ${pkg.version}`,
-    `date: ` + dateformat(null, DEFAULT_FORMAT),
-    `license: ${pkg.license}`,
+  const pairs = [
+    { key: 'name' },
+    { key: 'description' },
+    { key: 'homepage' },
+    { key: 'version' },
+    { key: 'date', value: dateformat(null, DEFAULT_FORMAT) },
+    { key: 'license' },
   ];
+
+  return pairs
+    .map((item) => {
+      const value = item.value || pkg[item.key];
+      return value ? `${item.key}: ${value}` : '';
+    })
+    .filter(Boolean);
 };
