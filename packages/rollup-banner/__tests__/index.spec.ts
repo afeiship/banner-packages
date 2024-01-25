187f3eb109ae038b88b20f206996613d5da9deb1
afeiship <1290657123@qq.com>
2024-01-25 08:41:27 +0800

    feat: upt rollup bannercomments

--- packages/rollup-banner/__tests__/index.spec.ts
+++ packages/rollup-banner/__tests__/index.spec.ts
@@ -1,7 +1,31 @@
-import fn from '../src';
+import rollupBanner from '../src';
 
 describe('api.basic', () => {
-  test('normail single value case', () => {
-    expect(fn({ rel: true })).toBe(undefined);
+  test('01/rollup-html comments', () => {
+    const pkg = require('../package.json');
+    const res = rollupBanner({ pkg, type: 'html' });
+    expect(typeof res).toBe('string');
+    expect(res.includes('<!--')).toBeTruthy();
   });
+
+  test('02/rollup-css comments', () => {
+    const pkg = require('../package.json');
+    const res = rollupBanner({ pkg, type: 'css' });
+    expect(typeof res).toBe('string');
+    expect(res.includes('/*')).toBeTruthy();
+  })
+
+  test('03/rollup-js comments', () => {
+    const pkg = require('../package.json');
+    const res = rollupBanner({ pkg, type: 'js' });
+    expect(typeof res).toBe('string');
+    expect(res.includes('/*')).toBeTruthy();
+  });
+
+  // pkg: false
+  test('04/rollup-js comments pkg/false', () => {
+    const res = rollupBanner({ type: 'js' });
+    expect(typeof res).toBe('string');
+    expect(res.includes('/*')).toBeTruthy();
+  })
 });
--- packages/rollup-banner/src/index.ts
+++ packages/rollup-banner/src/index.ts
@@ -1,17 +1,21 @@
+import fs from 'fs';
 import bannerDefs from '@jswork/banner-defaults';
+import nx from '@jswork/next';
 import '@jswork/next-nice-comments';
 
-type BannerType = 'html'|'css' | 'js';
+type BannerType = 'html' | 'css' | 'js';
+type JSONType = Record<string, any>;
 type RollupBannerOptions = {
   type?: BannerType
-  pkg: object
+  pkg?: JSONType;
 };
 
 const defaults = { type: 'js' };
 
 const rollupBanner = (inOptions: RollupBannerOptions): string => {
-  const { pkg, type } = { ...defaults, ...inOptions };
-  return nx.niceComments(bannerDefs(pkg), type)
+  let { pkg, type } = { ...defaults, ...inOptions };
+  if (!pkg) pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8')) as JSONType;
+  return nx.niceComments(bannerDefs(pkg), type);
 };
 
 export default rollupBanner;
