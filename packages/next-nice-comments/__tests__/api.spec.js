766636c0745c84352369833d4f6091739cee8991
afeiship <1290657123@qq.com>
2024-02-27 20:13:20 +0800

    refactor: nice comments use new code

--- packages/next-nice-comments/__tests__/api.spec.js
+++ packages/next-nice-comments/__tests__/api.spec.js
@@ -1,14 +1,12 @@
-(function () {
-  require('../src/index');
+require('../src/index');
 
-  describe('api.basic test', () => {
-    test('nx.niceComments', function () {
-      const comments = ['name: next-nice-comments', 'description:Just a lite', 'licence: MIT'];
+describe('api.basic test', () => {
+  test('nx.niceComments', function () {
+    const comments = ['name: next-nice-comments', 'description:Just a lite', 'licence: MIT'];
 
-      console.log(nx.niceComments(comments, 'js'));
-      console.log(nx.niceComments(comments, 'css'));
-      console.log(nx.niceComments(comments, 'html'));
-      console.log(nx.niceComments(comments, 'raw'));
-    });
+    console.log(nx.niceComments(comments, 'js'));
+    console.log(nx.niceComments(comments, 'css'));
+    console.log(nx.niceComments(comments, 'html'));
+    console.log(nx.niceComments(comments, 'raw'));
   });
-})();
+});
--- packages/next-nice-comments/src/index.js
+++ packages/next-nice-comments/src/index.js
@@ -1,38 +1,38 @@
-(function () {
-  var global = typeof window !== 'undefined' ? window : this || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var DEFAULT_TYPES = {
-    js: {
-      start: '/*!\n',
-      lineWith: ' * ',
-      end: '\n */\n\n'
-    },
-    css: {
-      start: '/*!\n',
-      lineWith: ' * ',
-      end: '\n */\n\n'
-    },
-    html: {
-      start: '<!--\n',
-      lineWith: ' * ',
-      end: '\n-->\n\n'
-    },
-    raw: {
-      start: '',
-      lineWith: ' ',
-      end: ''
-    }
-  };
+import nx from '@jswork/next';
 
-  nx.niceComments = function (inArray, inType) {
-    var options = DEFAULT_TYPES[inType] || inType;
-    var result = inArray.map(function (item) {
-      return options.lineWith + item;
-    });
-    return options.start + result.join('\n') + options.end;
-  };
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.niceComments;
+const DEFAULT_TYPES = {
+  js: {
+    start: '/*!\n',
+    lineWith: ' * ',
+    end: '\n */\n\n'
+  },
+  css: {
+    start: '/*!\n',
+    lineWith: ' * ',
+    end: '\n */\n\n'
+  },
+  html: {
+    start: '<!--\n',
+    lineWith: ' * ',
+    end: '\n-->\n\n'
+  },
+  raw: {
+    start: '',
+    lineWith: ' ',
+    end: ''
   }
-})();
+};
+
+nx.niceComments = function(inArray, inType) {
+  var options = DEFAULT_TYPES[inType] || inType;
+  var result = inArray.map(function(item) {
+    return options.lineWith + item;
+  });
+  return options.start + result.join('\n') + options.end;
+};
+
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = nx.niceComments;
+}
+
+export default nx.niceComments;
