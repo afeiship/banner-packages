f420011c6037a485c97e6263857d90cad3654047
afeiship <1290657123@qq.com>
2024-04-09 10:46:26 +0800

    feat: rm next-nice comments from this repo

--- packages/banner-defaults/src/index.ts
+++ packages/banner-defaults/src/index.ts
@@ -5,11 +5,12 @@ import readPkg from '@jswork/read-pkg-json';
 export default (inPkg?: Record<string, any>): string[] => {
   const pkg = inPkg || readPkg();
   const version = pkg.gtcVersion || pkg.version;
+  const _version = version === '0.0.0' ? undefined : version;
   const pairs = [
     { key: 'name' },
     { key: 'description' },
     { key: 'homepage' },
-    { key: 'version', value: version },
+    { key: 'version', value: _version },
     { key: 'date', value: sdf('datetime', es8date()) },
     { key: 'license' },
   ];
--- packages/next-nice-comments/index.d.ts
+++
@@ -1,9 +0,0 @@
-type CommentType = string | {
-  start: string;
-  lineWith: string;
-  end: string;
-}
-
-interface NxStatic {
-  niceComments: (target: string[], type: CommentType) => string;
-}
