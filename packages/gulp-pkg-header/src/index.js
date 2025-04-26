848b4371bf4ac8ee93451a1e07007ed1a973e690
afeiship <1290657123@qq.com>
2025-04-26 15:17:57 +0800

    feat: jsw nice comments

--- packages/gulp-pkg-header/src/index.js
+++ packages/gulp-pkg-header/src/index.js
@@ -5,12 +5,11 @@ var pkg = require(path.join(process.cwd(), './package.json'));
 var dateformat = require('dateformat');
 var DEFAULT_FORMAT = 'yyyy-mm-dd HH:MM:ss';
 var DEFAULT_OPTIONS = { type: 'js' };
-
-require('@jswork/next-nice-comments');
+var niceComments = require('@jswork/nice-comments');
 
 var generateComments = function (inType) {
   var type = inType || 'js';
-  return nx.niceComments(
+  return niceComments(
     [
       'name: <%= pkg.name %>',
       'description: <%= pkg.description %>',
--- packages/tsup-banner/src/index.ts
+++ packages/tsup-banner/src/index.ts
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import bfd from '@jswork/banner-defaults';
-import '@jswork/next';
-import '@jswork/next-nice-comments';
+import niceComments from '@jswork/nice-comments';
 
 type BannerType = 'html' | 'css' | 'js';
 type JSONType = Record<string, any>;
@@ -15,7 +14,7 @@ const defaults = { type: 'js' };
 const tsupBanner = (inOptions?: RollupBannerOptions): string => {
   let { pkg, type } = { ...defaults, ...inOptions };
   if (!pkg) pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8')) as JSONType;
-  return nx.niceComments(bfd(pkg), type);
+  return niceComments(bfd(pkg), type);
 };
 
 export default tsupBanner;
